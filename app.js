bb0ab4cc50b53523921f5928c08bdd1293faf0a3
Krikchou <31549619+Krikchou@users.noreply.github.com>
2021-01-21 12:14:57 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,80 +1,108 @@
 class bl1{
-    cnt=0;
+    cnt=-1;
     target;
 
     constructor(target){
-        this.cnt=2;
+        this.cnt=-1;
         this.target=target;
     }
 
-    aba(arr){
+    aba(arr, num){
+        if(this.cnt==-1 && num==6){
+            arr[2].appendChild(this.target);
+            this.cnt=2;
+        }else{
+        if(this.cnt>=0){
         if(this.cnt>=arr.length){
             this.cnt = this.cnt-arr.length;
         }    
-                arr[this.cnt].appendChild(this.target);
-            this.cnt++;
+                arr[this.cnt+num].appendChild(this.target);
+                this.cnt=this.cnt+num;
             console.log(this.cnt);
+        }
+    }
         }
 }
 
 class bl2{
-    cnt=0;
+    cnt=-1;
     target;
 
     constructor(target){
-        this.cnt=38;
+        this.cnt=-1;
         this.target=target;
     }
 
-    aba(arr){
+    aba(arr, num){
+        if(this.cnt==-1 && num==6){
+            arr[38].appendChild(this.target);
+            this.cnt=38;
+        }else{
+        if(this.cnt>=0){
         if(this.cnt>=arr.length){
             this.cnt = this.cnt-arr.length;
         }    
-                arr[this.cnt].appendChild(this.target);
-            this.cnt++;
+                arr[this.cnt+num].appendChild(this.target);
+                this.cnt=this.cnt+num;
             console.log(this.cnt);
+        }
+    }
         }
 }
 
 class bl3{
-    cnt=0;
+    cnt=-1;
     target;
 
     constructor(target){
-        this.cnt=14;
+        this.cnt=-1;
         this.target=target;
     }
 
-    aba(arr){
+    aba(arr,num){
+        if(this.cnt==-1 && num==6){
+            arr[14].appendChild(this.target);
+            this.cnt=14;
+        }else{
+            if(this.cnt>=0){
         if(this.cnt>=arr.length){
             this.cnt = this.cnt-arr.length;
         }    
-                arr[this.cnt].appendChild(this.target);
-            this.cnt++;
+                arr[this.cnt+num].appendChild(this.target);
+                this.cnt=this.cnt+num;
             console.log(this.cnt);
         }
+    }
+    }
 }
 
 class bl4{
-    cnt=0;
+    cnt=-1;
     target;
 
     constructor(target){
-        this.cnt=26;
+        this.cnt=-1;
         this.target=target;
     }
 
-    aba(arr){
+    aba(arr, num){
+        if(this.cnt==-1 && num==6){
+            arr[26].appendChild(this.target);
+            this.cnt=26;
+        }else{
+            if(this.cnt>=0){
         if(this.cnt>=arr.length){
             this.cnt = this.cnt-arr.length;
         }    
-                arr[this.cnt].appendChild(this.target);
-            this.cnt++;
+                arr[this.cnt+num].appendChild(this.target);
+                this.cnt=this.cnt+num;
             console.log(this.cnt);
         }
+    }
+    }
 }
 
-
+const dice=document.querySelector('#dice');
 const path=[document.querySelector('#midl').children[3].children[0],
             document.querySelector('#midl').children[3].children[4],
             document.querySelector('#midl').children[3].children[8],
@@ -156,21 +184,127 @@ const path=[document.querySelector('#midl').children[3].children[0],
             let bg=new bl4(document.querySelector('#grn2'));
             let cg=new bl4(document.querySelector('#grn3'));
             let dg=new bl4(document.querySelector('#grn4'));
+            var cnt=0;
+    
+       
+        
+        
+        
+        dice.addEventListener('click' ,function dicegen(){
+            var rand=Math.ceil( Math.random()*6);
+            console.log(rand);
+
+            switch(rand){
+            case 1: dice.children[0].style.opacity="-1";
+                dice.children[1].style.opacity="-1";
+                dice.children[2].style.opacity="-1";
+                dice.children[3].style.opacity="-1";
+                dice.children[4].style.opacity="1";
+                dice.children[5].style.opacity="-1";
+                dice.children[6].style.opacity="-1";
+                dice.children[7].style.opacity="-1";
+                dice.children[8].style.opacity="-1";
+
+                break;
+            case 2: dice.children[0].style.opacity="1";
+                dice.children[1].style.opacity="-1";
+                dice.children[2].style.opacity="-1";
+                dice.children[3].style.opacity="-1";
+                dice.children[4].style.opacity="-1";
+                dice.children[5].style.opacity="-1";
+                dice.children[6].style.opacity="-1";
+                dice.children[7].style.opacity="-1";
+                dice.children[8].style.opacity="1";
+
+                break;
+            case 3: dice.children[0].style.opacity="1";
+                dice.children[1].style.opacity="-1";
+                dice.children[2].style.opacity="-1";
+                dice.children[3].style.opacity="-1";
+                dice.children[4].style.opacity="1";
+                dice.children[5].style.opacity="-1";
+                dice.children[6].style.opacity="-1";
+                dice.children[7].style.opacity="-1";
+                dice.children[8].style.opacity="1";
+
+                break;
+            case 4: dice.children[0].style.opacity="1";
+                dice.children[1].style.opacity="-1";
+                dice.children[2].style.opacity="1";
+                dice.children[3].style.opacity="-1";
+                dice.children[4].style.opacity="-1";
+                dice.children[5].style.opacity="-1";
+                dice.children[6].style.opacity="1";
+                dice.children[7].style.opacity="-1";
+                dice.children[8].style.opacity="1";
+
+                break;
+            case 5: dice.children[0].style.opacity="1";
+                dice.children[1].style.opacity="-1";
+                dice.children[2].style.opacity="1";
+                dice.children[3].style.opacity="-1";
+                dice.children[4].style.opacity="1";
+                dice.children[5].style.opacity="-1";
+                dice.children[6].style.opacity="1";
+                dice.children[7].style.opacity="-1";
+                dice.children[8].style.opacity="1";
+
+                break;
+            case 6: dice.children[0].style.opacity="1";
+                dice.children[1].style.opacity="-1";
+                dice.children[2].style.opacity="1";
+                dice.children[3].style.opacity="1";
+                dice.children[4].style.opacity="-1";
+                dice.children[5].style.opacity="1";
+                dice.children[6].style.opacity="1";
+                dice.children[7].style.opacity="-1";
+                dice.children[8].style.opacity="1";
+
+                break;
+            default:dice.children[0].style.opacity="-1";
+                dice.children[1].style.opacity="-1";
+                dice.children[2].style.opacity="-1";
+                dice.children[3].style.opacity="-1";
+                dice.children[4].style.opacity="-1";
+                dice.children[5].style.opacity="-1";
+                dice.children[6].style.opacity="-1";
+                dice.children[7].style.opacity="-1";
+                dice.children[8].style.opacity="-1";
+                break;
+            }
+            if(cnt>3){
+                cnt=cnt-4;
+            }
+            switch(cnt){
+                case 0: alert("blues turn");
+                        bb1.addEventListener('click', function dd(){ a.aba(path, rand);});
+                        bb2.addEventListener('click', function dd(){ b.aba(path, rand);});
+                        bb3.addEventListener('click', function dd(){ c.aba(path, rand);});
+                        bb4.addEventListener('click', function dd(){ d.aba(path, rand);});
+                        break;
+                case 1: alert("reds turn");
+                        rr1.addEventListener('click', function dd(){ ar.aba(path, rand);});
+                        rr2.addEventListener('click', function dd(){ br.aba(path, rand);});
+                        rr3.addEventListener('click', function dd(){ cr.aba(path, rand);});
+                        rr4.addEventListener('click', function dd(){ dr.aba(path, rand);});   
+                        break;
+                case 2: alert("greens turn");
+                        gg1.addEventListener('click', function dd(){ ag.aba(path, rand);});
+                        gg2.addEventListener('click', function dd(){ bg.aba(path, rand);});
+                        gg3.addEventListener('click', function dd(){ cg.aba(path, rand);});
+                        gg4.addEventListener('click', function dd(){ dg.aba(path, rand);});
+                        break;
+                case 3: alert("yellows turn");
+                        yy1.addEventListener('click', function dd(){ ay.aba(path, rand);});
+                        yy2.addEventListener('click', function dd(){ by.aba(path, rand);});
+                        yy3.addEventListener('click', function dd(){ cy.aba(path, rand);});
+                        yy4.addEventListener('click', function dd(){ dy.aba(path, rand);});   
+                        break;
+            }
+            cnt++;
+
+
             
-            
-        bb1.addEventListener('click', function dd(){ a.aba(path);});
-        bb2.addEventListener('click', function dd(){ b.aba(path);});
-        bb3.addEventListener('click', function dd(){ c.aba(path);});
-        bb4.addEventListener('click', function dd(){ d.aba(path);});
-        yy1.addEventListener('click', function dd(){ ay.aba(path);});
-        yy2.addEventListener('click', function dd(){ by.aba(path);});
-        yy3.addEventListener('click', function dd(){ cy.aba(path);});
-        yy4.addEventListener('click', function dd(){ dy.aba(path);});
-        rr1.addEventListener('click', function dd(){ ar.aba(path);});
-        rr2.addEventListener('click', function dd(){ br.aba(path);});
-        rr3.addEventListener('click', function dd(){ cr.aba(path);});
-        rr4.addEventListener('click', function dd(){ dr.aba(path);});
-        gg1.addEventListener('click', function dd(){ ag.aba(path);});
-        gg2.addEventListener('click', function dd(){ bg.aba(path);});
-        gg3.addEventListener('click', function dd(){ cg.aba(path);});
-        gg4.addEventListener('click', function dd(){ dg.aba(path);});
+
+        });
+
